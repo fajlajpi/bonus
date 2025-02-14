73795900b63d7dcba5eff25e78ca2cf6ee8291ac
Filip Huta <fajlajpi@gmail.com>
2025-02-14 13:13:41 +0100

    Initial models

--- pa_bonus/models.py
+++ pa_bonus/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    pass
+
+class PointsTransaction(models.Model):
+    pass
+
+class ClientBrand(models.Model):
+    pass
+
+class PointsBalance(models.Model):
+    pass
+
