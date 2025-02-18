2c73ea0ca196521af1c887fd57a27ff18e942f1c
Filip Huta <fajlajpi@gmail.com>
2025-02-18 18:53:26 +0100

    Registered models to admin

--- pa_bonus/admin.py
+++ pa_bonus/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from pa_bonus import models
 
 # Register your models here.
+admin.site.register(models.User)
+admin.site.register(models.Brand)
+admin.site.register(models.UserContract)
+admin.site.register(models.PointsTransaction)
+admin.site.register(models.ContractBrands)
+admin.site.register(models.BrandBonus)
+admin.site.register(models.PointsBalance)
