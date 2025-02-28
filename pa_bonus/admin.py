ee9b47965fef8c277e6b16bbae37cf2348fe2b7c
Filip Huta <fajlajpi@gmail.com>
2025-03-01 00:32:18 +0100

    Improved admin registration of models

--- pa_bonus/admin.py
+++ pa_bonus/admin.py
@@ -1,14 +1,61 @@
 from django.contrib import admin
-from pa_bonus import models
+from import_export import resources, fields
+from import_export.widgets import DateWidget
+from django.contrib.auth.hashers import make_password
+from pa_bonus.models import (
+    User, Brand, UserContract, PointsTransaction, BrandBonus, PointsBalance, 
+    FileUpload, Reward, RewardRequest, RewardRequestItem
+)
 
-# Register your models here.
-admin.site.register(models.User)
-admin.site.register(models.Brand)
-admin.site.register(models.UserContract)
-admin.site.register(models.PointsTransaction)
-admin.site.register(models.BrandBonus)
-admin.site.register(models.PointsBalance)
-admin.site.register(models.FileUpload)
-admin.site.register(models.Reward)
-admin.site.register(models.RewardRequest)
-admin.site.register(models.RewardRequestItem)
+
+# REGISTERING AND SETTING UP MODELS FOR DJANGO ADMIN
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'last_name', 'first_name', 'user_number', 'user_phone')
+    search_fields = ('username', 'email', 'last_name', 'user_number')
+    list_filter = ('is_staff', 'is_active')
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'prefix')
+    search_fields = ('name', 'prefix')
+
+@admin.register(UserContract)
+class UserContractAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'contract_date_from', 'contract_date_to', 'is_active')
+    search_fields = ('user_id__username', 'user_id__email', 'user_id__user_number')
+    list_filter = ('is_active', 'contract_date_from', 'contract_date_to')
+
+@admin.register(PointsTransaction)
+class PointsTransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'type', 'value', 'status', 'date', 'description')
+    search_fields = ('user__username', 'user__email', 'user__user_number')
+    list_filter = ('type', 'status', 'date')
+    readonly_fields = ('created_at',)
+
+@admin.register(BrandBonus)
+class BrandBonusAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand_id', 'points_ratio')
+    search_fields = ('name', 'brand_id__name')
+
+@admin.register(FileUpload)
+class FileUploadAdmin(admin.ModelAdmin):
+    list_display = ('status', 'uploaded_at', 'file', 'processed_at', 'uploaded_by')
+    list_filter = ('status', 'uploaded_at', 'uploaded_by')
+    readonly_fields = ('uploaded_at', 'processed_at', 'status', 'error_message')
+
+@admin.register(Reward)
+class RewardAdmin(admin.ModelAdmin):
+    list_display = ('abra_code', 'name', 'point_cost', 'brand', 'is_active')
+    list_filter = ('brand', 'is_active')
+    search_fields = ('abra_code', 'name')
+
+@admin.register(RewardRequest)
+class RewardRequestAdmin(admin.ModelAdmin):
+    list_display = ('user', 'requested_at', 'status', 'total_points')
+    list_filter = ('status',)
+    search_fields = ('user__username', 'user__email')
+
+@admin.register(RewardRequestItem)
+class RewardRequestItemAdmin(admin.ModelAdmin):
+    list_display = ('reward_request', 'reward', 'quantity', 'point_cost')
