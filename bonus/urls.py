e10c2ac0d982dca7f9b17e874d0489b175039c70
Filip Huta <fajlajpi@gmail.com>
2025-03-01 02:28:09 +0100

    Added redirection from home / to dashboard

--- bonus/urls.py
+++ bonus/urls.py
@@ -17,10 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from django.shortcuts import redirect
 from pa_bonus import views
+
+def home_redirect(request):
+    """Redirects the root URL to dashboard."""
+    return redirect('dashboard')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_redirect, name='home_redirect'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('upload/', views.upload_file, name='upload_file'),
