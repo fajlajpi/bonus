d616eeea52b0eee5316713c23515ddabba3b9527
Filip Huta <fajlajpi@gmail.com>
2025-04-29 09:46:14 +0200

    Fixed: Small fixes for the settings update

--- bonus/settings/base.py
+++ bonus/settings/base.py
@@ -20,7 +20,7 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Application definition
 
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,11 @@
-# import os
-# import sys
+import os
+import sys
 
-# path = '/home/iepgvjxg/bonus' # Absolute path
-# if path not in sys.path:
-#     sys.path.append(path)
+path = '/home/iepgvjxg/bonus' # Absolute path
+if path not in sys.path:
+    sys.path.append(path)
 
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'bonus.settings.production' # Replace with your project name
+os.environ['DJANGO_SETTINGS_MODULE'] = 'bonus.settings.production'
 
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
